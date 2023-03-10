f1ca51dd702b3d5370033d00cac2e702e7f0170f
Santiago Giraldo <69825324+sgiraldo2@users.noreply.github.com>
2023-03-09 18:19:52 -0600

    Added timestamps to counter
    Modified counter html, css, js

--- pages/counter/counterscript.js
+++ pages/counter/counterscript.js
@@ -1,25 +1,55 @@
 var count = 0;
-var countSpan = document.getElementById('count');
+      var countSpan = document.getElementById('count');
+      var beginTime, currentTime; // new variables to store timestamps
 
-function increment() {
-  count++;
-  countSpan.textContent = count;
-}
+      function increment() {
+        if (count === 0) {
+          beginTime = new Date(); // set begin time if count is 0
+        }
+        count++;
+        countSpan.textContent = count;
+        currentTime = new Date(); // update current time on each click
+        updateTimestamps();
+      }
 
-function decrement() {
-  count--;
-  countSpan.textContent = count;
-}
+      function decrement() {
+        if (count > 0) {
+          count--;
+          countSpan.textContent = count;
+          currentTime = new Date(); // update current time on each click
+          updateTimestamps();
+        }
+      }
 
-function reset() {
-  count = 0;
-  countSpan.textContent = count;
-}
+      function reset() {
+        count = 0;
+        countSpan.textContent = count;
+        beginTime = null; // reset timestamps
+        currentTime = null;
+        updateTimestamps();
+      }
 
-var incrementButton = document.getElementById('increment');
-var decrementButton = document.getElementById('decrement');
-var resetButton = document.getElementById('reset');
+      function updateTimestamps() {
+        var beginTimeDisplay = document.getElementById('begin-time');
+        var currentTimeDisplay = document.getElementById('current-time');
+        if (beginTime) {
+          beginTimeDisplay.textContent =
+            'Begin Time: ' + beginTime.toLocaleString();
+        } else {
+          beginTimeDisplay.textContent = '';
+        }
+        if (currentTime) {
+          currentTimeDisplay.textContent =
+            'Current Time: ' + currentTime.toLocaleString();
+        } else {
+          currentTimeDisplay.textContent = '';
+        }
+      }
 
-incrementButton.addEventListener('click', increment);
-decrementButton.addEventListener('click', decrement);
-resetButton.addEventListener('click', reset);
+      var incrementButton = document.getElementById('increment');
+      var decrementButton = document.getElementById('decrement');
+      var resetButton = document.getElementById('reset');
+
+      incrementButton.addEventListener('click', increment);
+      decrementButton.addEventListener('click', decrement);
+      resetButton.addEventListener('click', reset);
